1626cf1283813b6ff4920dc4c79b8e8bbe152f58
lizhengchao <lizc@theplant.jp>
2019-10-29 16:41:39 +0800

    Remove the uesless pages/index.js

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var React = require("react");
-var Home = function () { return (<p>
-    Welcome!, here is <strong>Zencol</strong> club
-  </p>); };
-exports["default"] = Home;
